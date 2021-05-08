05daf5ff35eab8f8a60cb96dd75bafd6ef6145da
saqlain1020 <saqlainprinters@gmail.com>
2021-05-08 22:43:51 +0500

    Burgermenu and more routing

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme)=>({
+    root:{
+    
+    }
+}))
+
+const Profile = () => {
+    const classes = useStyles();
+    
+    return (
+        <div className={classes.root}>
+            
+        </div>
+    )
+}
+
+export default Profile;
